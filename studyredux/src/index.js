7aea1eb38d5d70c4c523e2411374f218039bb7b8
peckbyte <503410224@qq.com>
2018-09-08 10:44:42 +0800

    4.1代码完成，redux的简单例子

--- studyredux/src/index.js
+++ studyredux/src/index.js
@@ -14,3 +14,18 @@ function counter(state=0, action) {
 const store = createStore(counter)
 
 console.log(store)
+
+const init = store.getState()
+console.log(init)
+
+
+// 添加监听函数
+function listener(){
+    const currentData = store.getState()
+    console.log(`现在的数值是${currentData}`)
+}
+
+store.subscribe(listener)
+
+//执行action
+store.dispatch({type:'减'})
