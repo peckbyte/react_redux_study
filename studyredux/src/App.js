1a83f099db433142944fc32a1a27d604da5582cc
peckbyte <503410224@qq.com>
2018-09-08 16:26:09 +0800

    4.6代码调试完成，浅尝react-redux

--- studyredux/src/App.js
+++ studyredux/src/App.js
@@ -1,29 +1,33 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
+import { addDATAasync, addDATA, removeDATA } from './index.redux'
 class Count extends React.Component{
     render() {
         const {
-            store,
+            addDATA,
+            addDATAasync,
+            removeDATA,
+            num,
         } = this.props
 
-        const { addDATA } = this.props
-        const { addDATAasync } = this.props
-        const { removeDATA } = this.props
-
-        const num = store.getState()
         return (
             <div>
                 <h1>数值是：{num}</h1>
-                <button onClick={() => store.dispatch(addDATA())}  id = "add">增加数值</button>
-                <button onClick={() => store.dispatch(addDATAasync())}  id = "add">2秒后数值</button>
-                <button onClick={() => store.dispatch(removeDATA())}  id = "remove">减少数值</button>,
+                <button onClick={addDATA}  id = "add">增加数值</button>
+                <button onClick={addDATAasync}  id = "add">2秒后数值</button>
+                <button onClick={removeDATA}  id = "remove">减少数值</button>,
             </div>
-
 
             )
     }
 }
 
+const mapStateToProps= (state) => {
+ return {num: state}
+}
+
+const actionCreate  = {addDATA, addDATAasync, removeDATA}
 
 
+Count = connect(mapStateToProps,actionCreate)(Count)
 export default Count
--- studyredux/src/index.js
+++ studyredux/src/index.js
@@ -3,17 +3,20 @@ import ReactDom from 'react-dom'
 import App from './App'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import { counter, removeDATA, addDATA, addDATAasync } from './index.redux'
+import { Provider } from 'react-redux'
+import { counter } from './index.redux'
 const store = createStore(counter,compose(
     applyMiddleware(thunk),
     window.devToolsExtension?window.devToolsExtension():f=>f
 ))
 
 
-function render() {
-    ReactDom.render(<App store={store} removeDATA={removeDATA} addDATA={addDATA} addDATAasync={addDATAasync}></App>,document.getElementById('root'))
-}
+ReactDom.render(
+    (
+        <Provider store={store}>
+          <App />
+        </Provider>
+    ),
+        document.getElementById('root')
+)
 
-render()
-
-store.subscribe(render)
