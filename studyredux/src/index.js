2e45bcdb851df5c753d645017ba4350913d99231
peckbyte <503410224@qq.com>
2018-09-09 15:11:55 +0800

    zero

--- studyredux/src/index.js
+++ studyredux/src/index.js
@@ -4,10 +4,9 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
-// import { counter }  from './index.redux'
-import { Auth } from './Auth'
+
+
 import reduxs from './reduxs'
-import { Dashboard } from './Dashboard'
 import './config'
 import 'antd-mobile/dist/antd-mobile.css'
 const store = createStore(reduxs,compose(
@@ -15,17 +14,11 @@ const store = createStore(reduxs,compose(
     window.devToolsExtension?window.devToolsExtension():f=>f
 ))
 
-console.log(store)
 
 ReactDom.render(
     (
         <Provider store={store}>
             <BrowserRouter>
-                <Switch>
-                    <Route path='/login' exact component={Auth}/>
-                    <Route path='/dashboard'  component={Dashboard}/>
-                    <Redirect to='/dashboard' />
-                </Switch>
             </BrowserRouter>
         </Provider>
     ),
