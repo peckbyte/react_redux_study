21abae00548d11c3fabe302903ea0b61622de489
peckbyte <503410224@qq.com>
2018-09-12 00:17:18 +0800

    6.9代码完成，注册功能测试成功

--- studyredux/servers/user.js
+++ studyredux/servers/user.js
@@ -1,21 +1,36 @@
 const express = require('express')
 const Router = express.Router()
 const model =require('./model')
-const user = model.getModel('user')
+const User = model.getModel('user')
 
 
 Router.get('/list',(req,res) => {
-   user.find({}, function (err,doc) {
+   User.find({}, function (err,doc) {
        return res.json(doc)
    })
 })
+
 
 Router.get('/register',(req,res)=>{
    return res.json({code:1})
 })
 
-Router.post('/register',(req,res)=>{
-   return res.json({code:1})
+Router.post('/register',function(req,res){
+    console.log("haha")
+    console.log(req.header)
+   const {user,psw,role} = req.body
+   // const {user,psw,role} = {user:'peck',psw:'123',role:'boss'}
+    User.findOne({user},(e,d) => {
+        if (d) {
+            return res.json({code:1, msg:'用户名重复'})
+        }
+        User.create({user, psw, role},function (e, d) {
+            if (e) {
+                return res.json({code:1,msg:'服务端出错'})
+            }
+            return res.json({code:0})
+        })
+    })
 })
 
 module.exports = Router
